35cff73316c772ae2c67ef7c036102d6d5799368
Gaurav Kumar <gaurav31120@gmail.com>
2024-03-12 23:16:26 +0530

    5-> 2.Component, Props, JSX (iii) Link one file with another

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
-import "./App.css";
+import Video from './components/Video';
 
 function App() {
-  {/* Declaring variable  */}
-  let name = "react app"; 
-  let className = "App-header";
-  return (
-    <div className="">
-      <div className={className}>
-        {/* css added */}
-        {name}  {/* It will return name = react app*/}
-      {/* Using variables inside return */} 
-        <Demo></Demo>
-        {/* Attach other functions by using tags inside parent function and 
-      declaring the new function after the parent function */}
-      </div>
-    </div>
-  );
+  return  (
+    <div>
+  <div>Hello</div>
+  <Video></Video>
+  </div>
+  )
+
 }
 
-function Demo() {
-  return (
-    <div className="App">
-      <div className="App-header">Demo</div>
-    </div>
-  );
-}
+
 
 export default App;
