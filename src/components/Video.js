140eaa2ea910a6959bfd23f00930e8e9b52c0a1a
Gaurav Kumar <gaurav31120@gmail.com>
2024-03-16 23:59:44 +0530

    10-> 2.Component, Props, JSX (vi) Adding some more props

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,22 +1,24 @@
 import "./Video.css";
 
-// function Video(props) 
-function Video({title})
-{
+// function Video(props)
+function Video({ title, channel, views, time }) {
   console.log("v", Video);
 
-  let bg = "dark";
   return (
     <>
-      <div>
-        <img src="https://loremflickr.com/140/160" alt="Background img" />
-      </div>
-      <div className={bg} style={{ backgroundColor: "" }}>
-        {/* {props.title}  */}
-        {title}
-        {/* props.title is written for calling title props. */}
+      <div className="container">
+        <div className="pic">
+          <img src="https://loremflickr.com/140/160" alt="Background img" />
+        </div>
+        <div className="title">{title}</div>
+        <div className="channel">{channel}</div>
+        <div className="views">
+          {views} views <span>.</span>
+          {time}
+        </div>
       </div>
     </>
   );
 }
+
 export default Video;
