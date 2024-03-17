c33fcb02f9be4c14105407b0a113323366a3b88b
Gaurav Kumar <gaurav31120@gmail.com>
2024-03-17 16:45:05 +0530

    14-> 3.Conditional rendering, maps (ii) Conditional rendering by ternary operator

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,15 +1,16 @@
 import "./Video.css";
-
 // function Video(props)
 function Video({ title, channel = "gaurav", views, time, verified }) {
   // Here, we set a default value channel="gaurav". gaurav is default value.
   // let verified = true;
-  let channelJSX;
-  if (verified) {
-    channelJSX = <div className="channel">{channel} ✅</div>;
-  } else {
-    channelJSX = <div className="channel">{channel} </div>;
-  }
+
+  // let channelJSX;
+  // if(verified){
+  //   channelJSX =  <div className="channel">{channel} ✅</div>
+  // }
+  // else {
+  //   channelJSX =  <div className="channel">{channel} </div>
+  // }
 
   console.log("v", Video);
   return (
@@ -19,7 +20,11 @@ function Video({ title, channel = "gaurav", views, time, verified }) {
           <img src="https://picsum.photos/id/2/160/90" alt="Background img" />
         </div>
         <div className="title">{title}</div>
-        {channelJSX}
+
+        <div className="channel">
+          {channel} {verified ? "✅" : ""}{" "}
+        </div>
+        {/* {channelJSX} */}
         <div className="views">
           {views} <span>.</span> {time}
         </div>
