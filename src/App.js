5776bb5ac916b8d60923463dd13758c7dc6b9a54
Gaurav Kumar <gaurav31120@gmail.com>
2024-09-24 08:47:26 +0530

    23-> 8 - useReducers for State management

--- src/App.js
+++ src/App.js
@@ -3,41 +3,54 @@ import "./App.css";
 import videoDB from "./data/data";
 // import PlayButton from "./components/PlayButton";
 // import Counter from "./components/Counter";
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import AddVideo from "./components/AddVideo";
 import VideoList from "./components/VideoList";
 function App() {
-  const [videos, setVideos] = useState(videoDB);
   const [editableVideo, setEditableVideo] = useState(null);
+  function videoReducer(videos, action) {
+    switch (action.type) {
+      case "ADD":
+        return [...videos, { ...action.payload, id: videos.length + 1 }];
+      case "DELETE":
+        return videos.filter((video) => video.id !== action.payload);
+      case "UPDATE":
+        const index = videos.findIndex((v) => v.id === action.payload.id);
+        const newVideos = [...videos];
+        newVideos.splice(index, 1, action.payload);
+        setEditableVideo(null);
+        return newVideos;
+      default:
+        return videos;
+    }
+  }
+  const [videos, dispatch] = useReducer(videoReducer, videoDB);
+  // const [videos, setVideos] = useState(videoDB);
 
   function addVideos(video) {
-    setVideos([
-      ...videos,
-      { ...video, id: videos.length + 1 },
-      // {
-      //   id: videos.length + 1,
-      //   title: "Demo Js tutoriail",
-      //   views: "2M",
-      //   time: "3 years ago",
-      //   channel: "Coder Dost",
-      //   verified: true,
-      // },
-    ]);
+    dispatch({ type: "ADD", payload: video });
+    // action : {type:'ADD',payload:video}
+    // setVideos([
+    //   ...videos,
+    //   { ...video, id: videos.length + 1 },
+    // ]);
   }
 
   function deleteVideo(id) {
-    setVideos(videos.filter((video) => video.id !== id));
+    dispatch({ type: "DELETE", payload: id });
+    // setVideos(videos.filter((video) => video.id !== id));
     console.log(id);
   }
   function editVideo(id) {
     setEditableVideo(videos.find((video) => video.id === id));
   }
   function updateVideo(video) {
-    const index = videos.findIndex((v) => v.id === video.id);
-    const newVideos = [...videos];
-    newVideos.splice(index, 1, video);
-    setVideos(newVideos);
-    console.log(newVideos);
+    dispatch({ type: "UPDATE", payload: video });
+    // const index = videos.findIndex((v) => v.id === video.id);
+    // const newVideos = [...videos];
+    // newVideos.splice(index, 1, video);
+    // setVideos(newVideos);
+    // console.log(newVideos);
   }
   return (
     <>
