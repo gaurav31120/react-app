37160605473687a822b461451e5faa6ec2771939
Gaurav Kumar <gaurav31120@gmail.com>
2024-09-25 12:05:07 +0530

    35-> 13 - useMemo, useCallback, memo ---- useMemo basics

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,9 +1,17 @@
-import { useRef, useState } from "react";
+import { useMemo, useRef, useState } from "react";
+
+function fib(n) {
+  //1,2,3,4,5
+  if (n === 1 || n === 2) {
+    return 1;
+  }
+  return fib(n - 1) + fib(n - 2);
+}
 
 function Counter() {
   console.log("render counter");
   //   let number = 0;
-  const [number, setNumber] = useState(0);
+  const [number, setNumber] = useState(40);
   let num = useRef(0);
 
   function handleClick(e) {
@@ -15,9 +23,13 @@ function Counter() {
     console.log(num.current);
   }
 
+  const fibMemoized = useMemo(() => fib(number), [number]);
+
   return (
     <>
-      <h1 style={{ color: "white" }}>{number}</h1>
+      <h1 style={{ color: "white" }}>
+        {number} | {fibMemoized}
+      </h1>
       <button onClick={handleClick}>Add</button>
     </>
   );
